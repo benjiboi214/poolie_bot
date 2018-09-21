177f8acb5ec5b7911c9fd11d496b58831a6ba134
Ben Elliot <belliot@liveperson.com>
2018-09-21 16:15:54 +1000

    Handling draw actions

--- functions/draw.py
+++ functions/draw.py
@@ -3,9 +3,10 @@ import sys
 here = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(here)
 
+import requests
 from playhouse.shortcuts import model_to_dict
 
-from database.models import Competition, User
+from database.models import Competition, User, Draw
 from database.util import DatabaseManager
 
 from slack import generate_button_attachment
@@ -66,6 +67,17 @@ def generate_draw(event, context):
                 else:
                     print("Wew, we have collected all the info we need!")
                     pass
+                    # Get a count of the competitors
+                    # Determine the number of rounds
+                    # 
+
+
+
+
+
+
+
+
             else:
                 # User is not the admin. Advise them.
                 event['action_event'] = get_admin_error_message(user, competition)
@@ -76,8 +88,61 @@ def generate_draw(event, context):
 
 def select_draw_times(event, context):
     print("select_draw_times: Receieved event - ", event)
-    pass
+
+    with DatabaseManager("select_draw_times") as db:
+        user = User.select().where((User.slack_id == event["user"]["id"])).first()
+        competition = db.get_active_competition(event['actions'][0]["value"])
+
+        if competition and competition.status == Competition.GENERATE_DRAW:
+            if user_is_admin_of_competition(user, competition):
+                # Update the competition and trigger next action in flow
+                draw = Draw.get(Draw.competition == competition)
+                draw.times_around = int(event['sub_action'])
+                draw.save()
+
+                response_message = event['original_message']
+                del response_message['attachments'][0]['actions']
+                response_message['attachments'][0]['footer'] = "Roster will play {} time/s".format(event['sub_action'])
+
+                print("select_draw_times: Replacing action message with: ", response_message)
+                requests.post(event['response_url'], json=response_message)
+            else:
+                # User is not the admin. Advise them.
+                event['action_event'] = get_admin_error_message(user, competition)
+        else:
+            # No active competition found
+            event['action_event'] = get_competition_not_found_message(user)
+    return event
 
 def select_draw_finals(event, context):
     print("select_draw_finals: Receieved event - ", event)
-    pass
+
+    with DatabaseManager("select_draw_finals") as db:
+        user = User.select().where((User.slack_id == event["user"]["id"])).first()
+        competition = db.get_active_competition(event['actions'][0]["value"])
+
+        if competition and competition.status == Competition.GENERATE_DRAW:
+            if user_is_admin_of_competition(user, competition):
+                # Update the competition and trigger next action in flow
+                draw = Draw.get(Draw.competition == competition)
+                if event['sub_action'] == 'confirm':
+                  draw.finals = True
+                  message = "Finals will be played at the end of the season."
+                else:
+                  draw.finals = False
+                  message = "Finals will not be played at the end of the season."
+                draw.save()
+
+                response_message = event['original_message']
+                del response_message['attachments'][0]['actions']
+                response_message['attachments'][0]['footer'] = message
+
+                print("select_draw_finals: Replacing action message with: ", response_message)
+                requests.post(event['response_url'], json=response_message)
+            else:
+                # User is not the admin. Advise them.
+                event['action_event'] = get_admin_error_message(user, competition)
+        else:
+            # No active competition found
+            event['action_event'] = get_competition_not_found_message(user)
+    return event
